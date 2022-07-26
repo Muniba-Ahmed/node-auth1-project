dfbd36cde7af413bccf015d552af76496da1e882
Muniba Ahmed <mahmed93@berkeley.edu>
2022-07-25 18:40:57 -0700

    wip 5/15

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -39,10 +39,16 @@ async function checkUsernameFree(req, res, next) {
     "message": "Invalid credentials"
   }
 */
-function checkUsernameExists(req, res, next) {
-  next();
+async function checkUsernameExists(req, res, next) {
+  try {
+    const users = await User.findBy({ username: req.body.username });
+    if (!users.length) {
+      next();
+    } else next({ message: "Invalid credentials", status: 401 });
+  } catch (err) {
+    next(err);
+  }
 }
-
 /*
   If password is missing from req.body, or if it's 3 chars or shorter
 
@@ -52,7 +58,11 @@ function checkUsernameExists(req, res, next) {
   }
 */
 function checkPasswordLength(req, res, next) {
-  next();
+  if (!req.body.password || req.body.password.length < 3) {
+    next({ message: "Password must be longer than 3 chars", status: 422 });
+  } else {
+    next();
+  }
 }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
